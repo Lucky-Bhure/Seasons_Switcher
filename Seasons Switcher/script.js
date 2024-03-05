512795c05722592dd228eeccb9f24b71cb124352
Lucky Krushnarao Bhure <93819707+Lucky-Bhure@users.noreply.github.com>
2024-03-05 21:42:23 +0530

    Add files via upload

---
+++ Seasons Switcher/script.js
@@ -0,0 +1,18 @@
+let mainimage = document.getElementById("seasonimage");
+
+
+function spring(){
+    mainimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+}
+
+function summer(){
+    mainimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+}
+
+function autumn(){
+    mainimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+}
+
+function winter(){
+    mainimage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+}
